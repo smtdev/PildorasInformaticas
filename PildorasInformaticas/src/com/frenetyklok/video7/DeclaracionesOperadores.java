5364f9f4860c945d062794b03913ad0e077172e7
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2016-11-02 21:01:30 +0100

    video 7 terminado

---
+++ PildorasInformaticas/src/com/frenetyklok/video7/DeclaracionesOperadores.java
@@ -0,0 +1,30 @@
+package com.frenetyklok.video7;
+
+public class DeclaracionesOperadores {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b;
+		
+		b = 7; //aquí no tiene sentido utilizar dos líneas en la declaración de la variable.
+		
+		int c = b + a;
+		
+		c++;
+		c += 6;
+		
+		System.out.println(c);
+		
+		
+		int d = 16;
+		int g = 3;
+		
+		double f = d / g; // si queremos que conserve decimlaes numerador y/o denominador tienen que tenerlos
+		
+		System.out.println(f);
+		
+
+	}
+
+}

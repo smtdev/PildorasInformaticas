a10a3eb4df52de74eea16b21dec0ebceb3e1ba30
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2016-11-04 19:18:31 +0100

    Acabamos el video 9 con la clase Math

---
+++ PildorasInformaticas/src/com/frenetyklok/video8/DeclaracionVariables.java
@@ -0,0 +1,24 @@
+package com.frenetyklok.video8;
+
+public class DeclaracionVariables {
+
+	public static void main(String[] args) {
+		
+		/*
+		final int c = 5;
+		c = 7; //tenemos un error porque no podemos modificar el valor de la constante
+		
+		System.out.println(c);
+		*/
+		
+		final double apulgadas = 2.54;
+		
+		double centimetros = 6;
+		
+		double resultado = centimetros / apulgadas;
+		
+		System.out.println("En " + centimetros + " cm hay " + resultado + " pulgadas.");
+		
+	}
+
+}
---
+++ PildorasInformaticas/src/com/frenetyklok/video9/CalculosConMath.java
@@ -0,0 +1,13 @@
+package com.frenetyklok.video9;
+
+public class CalculosConMath {
+
+	public static void main(String[] args) {
+		
+		//int raiz = Math.sqrt(9); //esto da un erro porque el método devuelve un tipo double
+		
+		double raiz = Math.sqrt(9);// el metodo recibe un parametro double con lo que toma como 9.0
+		System.out.println(raiz);
+	}
+
+}

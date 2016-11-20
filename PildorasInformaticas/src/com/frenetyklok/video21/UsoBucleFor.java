ec85d5027a9de2ef3608a98d122840d113d0ddb9
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2016-11-20 15:08:48 +0100

    Vuelto a acabar el video 21

---
+++ PildorasInformaticas/src/com/frenetyklok/video21/UsoBucleFor.java
@@ -0,0 +1,25 @@
+package com.frenetyklok.video21;
+
+public class UsoBucleFor {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++){
+			System.out.println("Pablo");
+		}
+		
+		System.out.println("También hacia atrás!");
+		
+		for(int i = 10; i > 0; i--){
+			System.out.println("Pablo");
+		}
+		
+		System.out.println("Otra forma de hacer lo mismo:");
+		
+		for(int i = 0; i < 20; i += 2){
+			System.out.println("Pablo");
+		}
+
+	}
+
+}

549f019e7c8f174686512a2a3a142b35f747834a
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-01-23 15:11:59 +0100

    Prueba EGit

---
+++ PildorasInformaticas/src/com/frenetyklok/pruebaEgit/PruebaEgit.java
@@ -0,0 +1,9 @@
+package com.frenetyklok.pruebaEgit;
+
+public class PruebaEgit {
+	
+	public static void main(String[] args){
+		System.out.println("Esta es una prueba para ver si funciona la bajada con GIT!");
+	}
+
+}

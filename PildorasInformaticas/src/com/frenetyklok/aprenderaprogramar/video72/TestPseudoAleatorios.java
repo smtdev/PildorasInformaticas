4ee2fe129746862834f069b27f46fd06b0e31676
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-01-26 20:44:27 +0100

    Ejercicio 72 acabado

--- PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video72/TestPseudoAleatorios.java
+++ PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video72/TestPseudoAleatorios.java
@@ -6,7 +6,7 @@ public class TestPseudoAleatorios {
 	
 	public static void main(String[] args){
 		
-		System.out.println("Arranca el programa!");
+		System.out.println("Arranca el programa!. Por favor, pulse una tecla para iniciar las acciones.");
 		Integer tmpInteger = 0;
 		EntradaDeTeclado entradaMain = new EntradaDeTeclado();
 		
@@ -21,14 +21,19 @@ public class TestPseudoAleatorios {
 			SerieDeAleatorios serieDePrueba = new SerieDeAleatorios((int) tmpInteger);
 			serieDePrueba.generarSerieDeAleatorios();
 			serieDePrueba.mostrarSerie();
+			
+			//CAMBIAMOS EL VALOR DE TECLA PARA DECIDIR MAS ADELANTE SI SEGUIMOS O NO EN EL BUCLE
 			tecla = ' ';
 			System.out.println("Generar otra serie? (S/N)");
 			
 			while(tecla != 'n' && tecla != 'N' && tecla != 's' && tecla != 'S'){
+				
+				//HASTA QUE NOS DE UN VALOR ADECUADO SEGUIMOS PIDIENDO ENTRADA
 				entradaMain.pedirEntrada();
 			
 			
 				//OJO TENEMOS QUE COMPARAR CON EQUALS PORQUE LOS STRINGS SON OBJETOS
+				//EN EL MOMENTO QUE RECIBIMOS UN VALOR ADECUADO CONTINUAMOS EN EL WHILE EXTERIOR O SALIMOS DE EL
 			
 				if(entradaMain.getEntrada().equals("n") || entradaMain.getEntrada().equals("N")){
 					tecla = 'N';

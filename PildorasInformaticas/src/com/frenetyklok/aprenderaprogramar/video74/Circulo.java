ba8bfe9d8e1449b81541f67425faaede48230b67
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-01-27 22:00:48 +0100

    Acabado el video 74. Atención, falta el 73 que no está en los apuntes.

---
+++ PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video74/Circulo.java
@@ -0,0 +1,30 @@
+package com.frenetyklok.aprenderaprogramar.video74;
+
+public class Circulo {
+	
+	private static final double  PI = 3.1416;
+	private double radio;
+	
+	public Circulo(double radio){
+		this.radio = radio;
+	}
+	
+	public void setRadio(int radio){
+		this.radio = radio;
+	}
+	
+	public double getRadio(){
+		return this.radio;
+	}
+	
+	public double areaCirculo(){
+		return this.radio * this.radio * PI;
+	}
+	
+	public double longitudCirculo(){
+		return this.radio * 2 * PI;
+	}
+	
+	
+
+}
---
+++ PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video74/TestCirculo.java
@@ -0,0 +1,23 @@
+package com.frenetyklok.aprenderaprogramar.video74;
+import com.frenetyklok.aprenderaprogramar.video61.*;
+
+public class TestCirculo {
+	
+	public static void main(String[] args){
+		System.out.println("Empieza el programa.");
+		System.out.println("Por favor introduce el valor de la circunferencia: ");
+		
+		EntradaDeTeclado entrada = new EntradaDeTeclado();
+		
+		double radio = Double.parseDouble(entrada.getEntrada());
+		
+		Circulo circulo = new Circulo(radio);
+		
+		double area = circulo.areaCirculo();
+		double longitud = circulo.longitudCirculo();
+		
+		System.out.println("El círculo tiene de area " + area);
+		System.out.println("La circunferencia tiene de longitud: "  + longitud);
+	}
+
+}

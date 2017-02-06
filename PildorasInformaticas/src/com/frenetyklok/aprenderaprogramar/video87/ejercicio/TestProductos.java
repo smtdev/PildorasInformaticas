882bd23d2a07f32a780ed696609a4ebec4ebf2d5
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-02-06 14:17:57 +0100

    valoraciones sobre las herencias en el ejercicio 87

--- PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video87/ejercicio/TestProductos.java
+++ PildorasInformaticas/src/com/frenetyklok/aprenderaprogramar/video87/ejercicio/TestProductos.java
@@ -47,16 +47,28 @@ public class TestProductos {
 		/*
 		 * EL SIGUIENTE OBJETO NO TIENE LOS METODOS PROPIOS DE REFRIGERADO
 		 * PERO SI LO IMPRIMIMOS SI QUE USA TOSTRING DEFINIDO PARA REFRIGERADO
+		 * ESTO ES ASÍ PORQUE LA SUPERCLASE ADMITE TOSTRING PERO EN EJECUCIÓN 
+		 * SE BUSCA LA IMPLEMENTACION MAS CONCRETA, QUE EN ESTE CASO ES LA
+		 * DE LA SUBCLASE
+		 * APARECE TODA LA INFORMACION PORQUE AUNQUE NO ES ACCESIBLE CON LOS SET Y GET
+		 * DE LA SUPERCLASE, LOS PARAMETROS PROPIOS DE LA SUBCLASE SIGUEN ESTANDO GUARDADOS
+		 * EN EL OBJETO. PRODUCTO PRUEBA APUNTA A ESE OBJETO AUNQUE NO TIENE METODOS PARA
+		 * ACTUAR O RECUPERAR ESOS PARAMETROS ESPECIFICOS DE LA SUBCLASE
 		 */
 		
+		Producto productoPrueba = new ProductoRefrigerado(fechaCaducidad, 111, fechaEnvasado, "Madagascar", 111, "PDM");
+		System.out.println(productoPrueba);
 		/*
 		 * POR OTRO LADO SI HACEMOS UN CASTING SI QUE ESTARAN ACCESIBLE LOS METODOS
 		 */
 		
-		Producto productoPrueba = new ProductoRefrigerado(fechaCaducidad, 111, fechaEnvasado, "Madagascar", 111, "PDM");
+		
 		ProductoRefrigerado productoCasteado = (ProductoRefrigerado) productoPrueba;
 		productoCasteado.setCodigoSupervision("RDF");
 		System.out.println(productoCasteado);
+		
+		System.out.println("sigue estando productoPrueba?");//VEMOS QUE NO PORQUE APUNTA AL MISMO OBJETO QUE CASTEADO Y LE HEMOS CAMBIADO EL CODIGO
+		System.out.println(productoPrueba);
 	}
 
 }

aad9e3ccf440388ac33b93ff052eda16eafb75af
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-03-04 12:54:09 +0100

    Estamos en el tema 14 avanzado Aprenderaprogramar

---
+++ PildorasInformaticas/src/com/frenetyklok/avanzado/t11/ClaseStringBuffer.java
@@ -0,0 +1,31 @@
+package com.frenetyklok.avanzado.t11;
+
+/*
+ * LA CLASE STRINGBUILDER TIENE LOS MISMOS METODOS
+ * LA DIFERENCIA ES QUE STRINGBUFFER TIENE LOS METODOS
+ * SINCRONIZADOS LO QUE PERMITE TRABAJAR CON MULTIPLES HILOS
+ * DE EJECUCION. LA CLASE STRINBUILDER ES MAS EFECTIVA SI
+ * NO TENEMOS QUE TRABAJAR CON HILOS DE EJECUCION
+ */
+
+public class ClaseStringBuffer {
+	
+	public static void main(String[] args){
+		StringBuffer sb = new StringBuffer();
+		sb.append("Hola me llamo Pablo");
+		System.out.println(sb);
+		System.out.println(sb.length());
+		sb.deleteCharAt(1);
+		System.out.println(sb);
+		System.out.println(sb.length());
+		sb.insert(1, 'o');
+		System.out.println(sb);
+		sb.delete(1, 2);
+		System.out.println(sb);
+		sb.insert(1, 'o');
+		System.out.println(sb);
+		System.out.println(sb.substring(0, 4));
+		System.out.println(sb);
+	}
+
+}

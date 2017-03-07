deff5cf2bd84b1bdb73f8bdd0b4a0a2d0bb175e8
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-03-07 21:08:29 +0100

    Acabamos el tema 17 avanzado Aprenderaprogramar

---
+++ PildorasInformaticas/src/com/frenetyklok/avanzado/t17/ProgramaColeccionesArrayList.java
@@ -0,0 +1,28 @@
+package com.frenetyklok.avanzado.t17;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ProgramaColeccionesArrayList {
+	
+	public static void main(String[] args){
+		Collection listaMarcaCoches = new ArrayList<String>();
+		listaMarcaCoches.add("Audi");
+		listaMarcaCoches.add("Porsche");
+		listaMarcaCoches.add("Aston Martin");
+		listaMarcaCoches.add("Ferrari");
+		listaMarcaCoches.add("Mercedes");
+		listaMarcaCoches.add("Seat");
+		
+		System.out.println("Número de elementos antes de eliminar: " + listaMarcaCoches.size());
+		System.out.println(listaMarcaCoches.toString());
+		listaMarcaCoches.remove("Seat");
+		listaMarcaCoches.remove("Porsache");//si no escribimos bien no elimina
+		
+		System.out.println("Número de elementos tras eliminaciones: " + listaMarcaCoches.size());
+		System.out.println(listaMarcaCoches.toString());
+		
+		
+	}
+
+}

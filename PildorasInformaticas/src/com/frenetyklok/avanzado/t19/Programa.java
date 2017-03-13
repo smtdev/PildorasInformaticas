d5fa193cf038aba3c20d271708734c3da4e35d9f
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-03-13 21:48:55 +0100

    pequeño cambio

--- PildorasInformaticas/src/com/frenetyklok/avanzado/t19/Programa.java
+++ PildorasInformaticas/src/com/frenetyklok/avanzado/t19/Programa.java
@@ -2,6 +2,8 @@ package com.frenetyklok.avanzado.t19;
 
 import java.util.Iterator;
 
+import com.frenetyklok.avanzado.t19.ListaPersonas.MiIteradorListaPersonas;
+
 public class Programa {
 	
 	public static void main(String[] args){

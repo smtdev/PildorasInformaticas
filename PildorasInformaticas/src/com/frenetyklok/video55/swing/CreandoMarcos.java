1b26ecb069792b881ebf9d1b6348663a5d1c47b5
Frenetyklok <Frenetyklok@RUSH-DESKTOP>
2017-04-05 13:52:51 +0200

    VIDEO 57 TERMINADO SWING CENTRADO FRAME Y CAMBIANDO ICONO FRAME

---
+++ PildorasInformaticas/src/com/frenetyklok/video55/swing/CreandoMarcos.java
@@ -0,0 +1,31 @@
+package com.frenetyklok.video55.swing;
+
+import java.awt.Frame;
+
+import javax.swing.*;
+
+public class CreandoMarcos {
+	
+	public static void main(String[] args){
+		
+		MiMarco marcoUno = new MiMarco();
+		marcoUno.setVisible(true);//orden para que se vea el marco
+	}
+
+}
+
+
+class MiMarco extends JFrame{
+	
+	public MiMarco(){
+		//setSize(500, 300);//orden para dar tamaño al marco
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//comportamiento al cerrar el marco
+		//setLocation(500, 300);//localizamos el vértice superior izquierdo del Frame
+		
+		setBounds(500, 300, 800, 700);//sustituye location y size en este mismo orden
+		//setResizable(false);//para permitir o no redimensionar el marco. comportamiento por defecto es true
+		//setExtendedState(Frame.MAXIMIZED_BOTH);//el marco se abre a pantalla completa. Este método marca el estado al arrancar
+		setTitle("Mi Ventana");//para poner título a un marco
+	}
+	
+}
---
+++ PildorasInformaticas/src/com/frenetyklok/video55/swing/MarcoCentroPantalla.java
@@ -0,0 +1,44 @@
+package com.frenetyklok.video55.swing;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class MarcoCentroPantalla {
+
+	public static void main(String[] args) {
+		
+		MiMarcoCentrado mimarco = new MiMarcoCentrado();
+		mimarco.setVisible(true);
+
+	}
+
+}
+
+
+class MiMarcoCentrado extends JFrame{
+	
+	public MiMarcoCentrado(){
+		
+		Toolkit miPantalla = Toolkit.getDefaultToolkit();// almacenamos en miPantalla nuestro sistema de ventanas
+		Dimension resolucionPantalla = miPantalla.getScreenSize();//obtenemos la resolución de la pantalla nos devuelve un Dimension
+		
+		int alturaPantalla = resolucionPantalla.height;
+		int anchoPantalla = resolucionPantalla.width;
+		
+		//queremos una ventana de 500 x 400 en el centro de la pantalla
+		//el centro de la pantalla está en anchoPantalla/2 y alturaPantalla/2
+		
+		setBounds((anchoPantalla/2) - (500/2), (alturaPantalla/2) - (400/2), 500, 400);//ahora tenemos la ventana en el centro de la pantalla
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Ventana en el centro del monitor");
+		
+		Image miIcono = miPantalla.getImage("E:/Curso Java/logotipo.jpg");//se puede usar / o \\ y rutas relativas y absolutas
+		setIconImage(miIcono);
+		
+		
+	}
+	
+}
